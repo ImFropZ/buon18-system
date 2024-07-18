15f1c64526c1280dd8dcf44c8799e35d3e621381
LimTangmeng <limtangmeng100@gmail.com>
2024-07-18 11:25:36 +0700

    refactor: move auth guard outside of resources

--- src/app/(nav-layout)/layout.tsx
+++ src/app/(nav-layout)/layout.tsx
@@ -1,8 +1,16 @@
 import { Layout as BaseLayout } from "@components/layout";
+import { authProviderServer } from "@providers/auth-provider";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function Layout({ children }: React.PropsWithChildren) {
+  const data = await getData();
+
+  if (!data.authenticated) {
+    redirect(data?.redirectTo || "/login");
+  }
+
   const layout = cookies().get("nav:layout");
   const collapse = cookies().get("nav:collapse");
 
@@ -17,4 +25,14 @@ export default async function Layout({ children }: React.PropsWithChildren) {
       {children}
     </BaseLayout>
   );
+}
+
+async function getData() {
+  const { authenticated, redirectTo, error } = await authProviderServer.check();
+
+  return {
+    authenticated,
+    redirectTo,
+    error,
+  };
 }
