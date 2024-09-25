c2263604c2d84f91b19a4c8dd36257fc6aa89e92
LimTangmeng <limtangmeng100@gmail.com>
2024-09-25 17:06:29 +0700

    feat: add create feature on subjects page

--- src/modules/lobby-serksa/components/data-tables/subject-data-table.tsx
+++ src/modules/lobby-serksa/components/data-tables/subject-data-table.tsx
@@ -21,6 +21,236 @@ import React from "react";
 import { parseAsInteger, useQueryState } from "nuqs";
 import { axiosInstance } from "@modules/lobby-serksa/fetch";
 import { usePagination } from "@hooks";
+import * as z from "zod";
+import { CreateSubjectsSchema, Major } from "@modules/lobby-serksa/models";
+import { useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField } from "@components/ui/form";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@components/ui/sheet";
+import { toast } from "@components/ui/use-toast";
+import { Label } from "@components/ui/label";
+import { InputFormField, SearchSelectFormField } from "@components/form";
+
+async function onCreateHandler(data: z.infer<typeof CreateSubjectsSchema>) {
+  const subjects = data.subjects.map((subject) => {
+    return {
+      name: subject.name,
+      year: subject.year,
+      semester: subject.semester,
+      major_id: subject.major.id,
+    };
+  });
+  return axiosInstance.post(`/admin/subjects`, subjects).then((res) => {
+    return res.data as { code: number; message: string };
+  });
+}
+
+function CreateSubjectSheet({
+  children,
+  refetch,
+}: {
+  children: React.ReactNode;
+  refetch: () => void;
+}) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const form = useForm<z.infer<typeof CreateSubjectsSchema>>({
+    resolver: zodResolver(CreateSubjectsSchema),
+    defaultValues: {
+      subjects: [
+        { name: "", year: 0, semester: 0, major: { id: 0, name: "" } },
+      ],
+    },
+  });
+
+  const fieldArray = useFieldArray({
+    control: form.control,
+    name: "subjects",
+  });
+
+  return (
+    <Form {...form}>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>{children}</SheetTrigger>
+        <SheetContent className="lg:max-w-4xl">
+          <form
+            onSubmit={form.handleSubmit(
+              (d) => {
+                onCreateHandler(d)
+                  .then((response) => {
+                    form.reset();
+                    refetch();
+                    setIsOpen(false);
+                    toast({
+                      title: "Success",
+                      description: response.message,
+                    });
+                  })
+                  .catch((errRes) => {
+                    toast({
+                      title: "Failed to create subjects",
+                      description: errRes.response.data.message,
+                      variant: "destructive",
+                    });
+                  });
+              },
+              () => {
+                toast({
+                  title: "Invalid form data",
+                  description: "Problem with subjects data",
+                  variant: "destructive",
+                });
+              },
+            )}
+          >
+            <SheetHeader>
+              <SheetTitle>Create subjects</SheetTitle>
+              <SheetDescription>
+                Create multiple majors at once.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="my-2 flex flex-col gap-2">
+              {fieldArray.fields.map((field, index) => {
+                return (
+                  <div
+                    key={field.id}
+                    className="rounded border-2 p-2 px-4 pb-4"
+                  >
+                    <p className="ml-auto w-fit rounded bg-gray-300 px-2 text-sm">
+                      {field.id}
+                    </p>
+                    <Label>Name</Label>
+                    <FormField
+                      control={form.control}
+                      name={`subjects.${index}.name`}
+                      render={({ field }) => (
+                        <InputFormField
+                          field={field}
+                          errorField={
+                            form.formState.errors
+                              ? form.formState.errors.subjects?.[index]?.name
+                              : undefined
+                          }
+                          placeholder="Name"
+                        />
+                      )}
+                    />
+                    <div className="flex gap-4">
+                      <div className="flex-1">
+                        <Label>Semester</Label>
+                        <FormField
+                          control={form.control}
+                          name={`subjects.${index}.semester`}
+                          render={({ field }) => (
+                            <InputFormField
+                              field={field}
+                              errorField={
+                                form.formState.errors
+                                  ? form.formState.errors.subjects?.[index]
+                                      ?.semester
+                                  : undefined
+                              }
+                              placeholder="Semester"
+                            />
+                          )}
+                        />
+                      </div>
+                      <div className="flex-1">
+                        <Label>Year</Label>
+                        <FormField
+                          control={form.control}
+                          name={`subjects.${index}.year`}
+                          render={({ field }) => (
+                            <InputFormField
+                              field={field}
+                              errorField={
+                                form.formState.errors
+                                  ? form.formState.errors.subjects?.[index]
+                                      ?.semester
+                                  : undefined
+                              }
+                              placeholder="Semester"
+                            />
+                          )}
+                        />
+                      </div>
+                    </div>
+                    <Label>Major</Label>
+                    <FormField
+                      control={form.control}
+                      name={`subjects.${index}.major`}
+                      render={({ field }) => (
+                        <SearchSelectFormField
+                          id="major"
+                          field={field}
+                          errorField={
+                            form.formState.errors
+                              ? form.formState.errors.subjects?.[index]?.major
+                              : undefined
+                          }
+                          placeholder="Select Major"
+                          fetchResource={async (searchPhase) => {
+                            return axiosInstance
+                              .get(`/admin/majors`, {
+                                params: { ["name:ilike"]: searchPhase },
+                              })
+                              .then((res) => {
+                                return res.data.data.majors;
+                              });
+                          }}
+                          optionLabel="name"
+                          optionValue="id"
+                          onSelected={function (value: Major) {
+                            field.onChange({ id: value.id, name: value.name });
+                          }}
+                        />
+                      )}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex gap-2">
+              <Button
+                type="button"
+                onClick={() =>
+                  fieldArray.append({
+                    name: "",
+                    year: 0,
+                    semester: 0,
+                    major: { id: 0, name: "" },
+                  })
+                }
+              >
+                Add Subject
+              </Button>
+              {fieldArray.fields.length > 1 && (
+                <Button
+                  type="button"
+                  onClick={() =>
+                    fieldArray.remove(fieldArray.fields.length - 1)
+                  }
+                >
+                  Remove Last Subject
+                </Button>
+              )}
+            </div>
+            <SheetFooter>
+              <Button type="submit">Create</Button>
+            </SheetFooter>
+          </form>
+        </SheetContent>
+      </Sheet>
+    </Form>
+  );
+}
 
 export function SubjectDataTable() {
   const [limit, setLimit] = useQueryState(
@@ -71,7 +301,9 @@ export function SubjectDataTable() {
   return (
     <div className="grid h-full grid-rows-[auto,1fr,auto] gap-2 pb-4">
       <div className="flex justify-end gap-4">
-        <Button>Create</Button>
+        <CreateSubjectSheet refetch={refetch}>
+          <Button>Create</Button>
+        </CreateSubjectSheet>
       </div>
       <div className="overflow-hidden rounded-lg border">
         <Table>
--- src/modules/lobby-serksa/models/subject.ts
+++ src/modules/lobby-serksa/models/subject.ts
@@ -1,9 +1,22 @@
+import { numberInString } from "@models";
 import { Major } from "./major";
+import * as z from "zod";
 
 export interface Subject {
   id: number;
   name: string;
-  year: number;
   semester: number;
+  year: number;
   major: Major;
 }
+
+export const CreateSubjectSchema = z.object({
+  name: z.string(),
+  semester: numberInString.pipe(z.number()),
+  year: numberInString.pipe(z.number()),
+  major: z.object({ id: z.number(), name: z.string() }),
+});
+
+export const CreateSubjectsSchema = z.object({
+  subjects: z.array(CreateSubjectSchema),
+});
