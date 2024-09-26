442d7b9d5239c170b9ecdc7ef81a097787cf7532
LimTangmeng <limtangmeng100@gmail.com>
2024-09-26 17:01:35 +0700

    feat: add update feature on professors pages

--- src/modules/lobby-serksa/models/professor.ts
+++ src/modules/lobby-serksa/models/professor.ts
@@ -17,3 +17,11 @@ export const CreateProfessorSchema = z.object({
 export const CreateProfessorsSchema = z.object({
   professors: z.array(CreateProfessorSchema),
 });
+
+export const UpdateProfessorSchema = z.object({
+  title: z.enum(["Dr.", "Prof.", "Assoc. Prof.", "Asst. Prof."]),
+  full_name: z.string(),
+  subjects: z.array(z.object({ id: z.number(), name: z.string() })),
+  add_subjects: z.array(z.object({ id: z.number(), name: z.string() })),
+  remove_subjects: z.array(z.object({ id: z.number(), name: z.string() })),
+});
