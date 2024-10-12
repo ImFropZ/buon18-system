8f1d528d9dc3f4a3755cb48621aded73703dee5f
LimTangmeng <limtangmeng100@gmail.com>
2024-10-12 20:39:17 +0700

    feat: add delete users feature

--- src/modules/setting/components/data-tables/user-columns.tsx
+++ src/modules/setting/components/data-tables/user-columns.tsx
@@ -1,6 +1,19 @@
 "use client";
 
 import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@components/ui/alert-dialog";
+import { Button } from "@components/ui/button";
+import { Checkbox } from "@components/ui/checkbox";
+import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
@@ -8,12 +21,35 @@ import {
 } from "@components/ui/dropdown-menu";
 import { toast } from "@components/ui/use-toast";
 import { User } from "@modules/setting/models";
+import { systemAxiosInstance } from "@modules/shared";
 import { ColumnDef } from "@tanstack/react-table";
 import { Bot, Copy, MoreHorizontal, UserIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 export const userColumns: ColumnDef<User>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "id",
     header: "ID",
@@ -79,18 +115,78 @@ export const userColumns: ColumnDef<User>[] = [
   },
   {
     header: "Actions",
-    cell: ({ row }) => {
+    cell: ({ row, table }) => {
+      // NOTE: This is a hack to get the refetch function from the table options. Seems like the table meta is not being passed to the header component.
+      const meta = table.options.meta as { refetch: () => void } | undefined;
+
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <MoreHorizontal className="cursor-pointer" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem className="cursor-pointer" asChild>
-              <Link href={`/setting/users/edit/${row.original.id}`}>Edit</Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <MoreHorizontal className="cursor-pointer" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem className="cursor-pointer" asChild>
+                <Link href={`/setting/users/edit/${row.original.id}`}>
+                  Edit
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer" asChild>
+                <AlertDialogTrigger asChild>
+                  <div className="w-full text-red-400 hover:text-red-500 focus:text-red-500">
+                    Delete
+                  </div>
+                </AlertDialogTrigger>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                records.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={async () => {
+                  const response = await systemAxiosInstance
+                    .delete(`/setting/users`, {
+                      data: { ids: [row.original.id] },
+                    })
+                    .then((res) => {
+                      return res.data as { code: number; message: string };
+                    })
+                    .catch((err) => {
+                      return err.response.data as {
+                        code: number;
+                        message: string;
+                      };
+                    });
+
+                  if (response.code === 200) {
+                    toast({
+                      title: "Success",
+                      description: response.message,
+                    });
+
+                    meta?.refetch();
+                  } else {
+                    toast({
+                      title: "Error",
+                      description: response.message,
+                      variant: "destructive",
+                    });
+                  }
+                }}
+              >
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       );
     },
   },
