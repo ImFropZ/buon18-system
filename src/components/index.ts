abb3a7c735c22d4252b0b2117a4a3a7d8add87d2
LimTangmeng <limtangmeng100@gmail.com>
2024-10-12 20:44:57 +0700

    refactor: move `SearchPopover` to main component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export * from "./Layout";
 export * from "./SearchBar";
 export * from "./AdvanceSearch";
 export * from "./DeleteSelectButton";
+export * from "./SearchPopover";
--- src/modules/lobby-serksa/components/advance-searchs/MajorAdvanceSearch.tsx
+++ src/modules/lobby-serksa/components/advance-searchs/MajorAdvanceSearch.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { AdvanceSearch } from "@components";
+import { AdvanceSearch, SearchPopover } from "@components";
 import { Input } from "@components/ui/input";
 import { Label } from "@components/ui/label";
 import React from "react";
-import { SearchPopover } from "../SearchPopover";
 import { axiosInstance } from "@modules/lobby-serksa/fetch";
 import { Button } from "@components/ui/button";
 
