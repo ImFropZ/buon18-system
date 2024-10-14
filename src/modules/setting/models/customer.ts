8efb73b125f64512b8de8d8ba0f020eb615395f3
LimTangmeng <limtangmeng100@gmail.com>
2024-10-14 18:24:23 +0700

    feat: add create customer page fix: `SelectFormField` component unable to reset

--- src/modules/setting/models/customer.ts
+++ src/modules/setting/models/customer.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export interface Customer {
   id: number;
   full_name: string;
@@ -8,3 +10,13 @@ export interface Customer {
     [key: string]: any;
   };
 }
+
+export const CreateCustomerSchema = z.object({
+  full_name: z.string().min(1),
+  gender: z.enum(["m", "f", "u"], {
+    message: "You must select either one of the options.",
+  }),
+  email: z.string().email(),
+  phone: z.string().min(10),
+  additional_info: z.record(z.string(), z.any()),
+});
