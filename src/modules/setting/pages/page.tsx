4922cad94d7a0034e6b11d16677b968d0e9a52d6
LimTangmeng <limtangmeng100@gmail.com>
2024-10-17 13:28:47 +0700

    feat: view available permissions list on dashboard page

--- src/modules/setting/pages/page.tsx
+++ src/modules/setting/pages/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { PermissionSection } from "../components";
 
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <main className="relative grid h-full grid-rows-[auto,1fr] gap-2 p-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <PermissionSection />
+    </main>
+  );
 }
