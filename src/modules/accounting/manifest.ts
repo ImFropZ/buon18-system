16bb80bc661afc812fefba7ccbdd6f5214fb181d
LimTangmeng <limtangmeng100@gmail.com>
2024-10-23 14:30:46 +0700

    feat: init accouting module

---
+++ src/modules/accounting/manifest.ts
@@ -0,0 +1,23 @@
+import React from "react";
+import { routes } from "./routes";
+import { Manifest } from "@modules/inteface";
+
+export const manifest: Manifest = {
+  name: "accounting",
+  displayName: "Accounting",
+  description: "",
+  rootPath: "/accounting",
+  icon: "/modules/accounting.jpg",
+  routes: routes,
+  dataProvider: null,
+  pages: [
+    {
+      key: "/accounting",
+      path: React.lazy(() => import("./pages/page")),
+    },
+    {
+      key: "/accounting/payment-terms",
+      path: React.lazy(() => import("./pages/payment-terms/page")),
+    },
+  ],
+};
